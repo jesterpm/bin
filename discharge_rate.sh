b618211b21c2d89910b031ccdc4637f5dd1b3ae9
Jesse Morgan <jesse@jesterpm.net>
2022-04-02 12:25:47 -0700

    Add discharge_rate.sh

---
+++ discharge_rate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo - | awk "{printf \"%.1f\", \
+$(( \
+  $(cat /sys/class/power_supply/BAT0/current_now) * \
+  $(cat /sys/class/power_supply/BAT0/voltage_now) \
+)) / 1000000000000 }" ; echo " W"
+
